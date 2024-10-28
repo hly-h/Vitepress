2b15957eba9e376fc342f5317ec8fd61f1f56ffd
moumou <1482283801@qq.com>
2024-10-28 18:36:04 +0800

    feat(custom): action

--- dist/button/index.js
+++ dist/button/index.js
@@ -1,5 +1,4 @@
 import { defineComponent, createVNode } from "vue";
-const version = "1.0.1";
 const componentPrefix = "S";
 function withInstall(options) {
   options.install = (app) => {
@@ -89,64 +88,8 @@ const _Button = /* @__PURE__ */ defineComponent({
   }
 });
 const Button = withInstall(_Button);
-const linkProps = {
-  size: {
-    type: String,
-    default: "medium"
-  },
-  color: {
-    type: String,
-    default: "purple"
-  }
-};
-const _Link = /* @__PURE__ */ defineComponent({
-  name: "Link",
-  props: linkProps,
-  setup(props, {
-    slots
-  }) {
-    const size = {
-      small: {
-        x: "2",
-        y: "1",
-        text: "sm"
-      },
-      medium: {
-        x: "3",
-        y: "1.5",
-        text: "base"
-      },
-      large: {
-        x: "4",
-        y: "2",
-        text: "lg"
-      }
-    };
-    return () => createVNode("a", {
-      "class": ` 
-              hover:text-white
-              cursor-pointer
-              py-${size[props.size].y}
-              px-${size[props.size].x}
-              text-${`${props.color}-500`}
-              text-${size[props.size].text}
-              hover:bg-${props.color}-400
-              `
-    }, [createVNode("span", null, [" ", slots.default ? slots.default() : "默认链接", " "])]);
-  }
-});
-const Link = withInstall(_Link);
-const plugins = [Link, Button];
-const entry = {
-  install(app) {
-    plugins.forEach((c) => app.use(c));
-  },
-  version
-};
 export {
   Button,
-  Link,
   buttonProps,
-  entry as default,
-  linkProps
+  Button as default
 };
---
+++ dist/entry.d.ts
@@ -0,0 +1,11 @@
+import type { Plugin } from 'vue';
+import 'virtual:uno.css';
+export * from './button';
+export * from './link';
+declare const _default: {
+    install(app: {
+        use: (arg0: Plugin) => void;
+    }): void;
+    version: string;
+};
+export default _default;
